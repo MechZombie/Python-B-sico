8cc28f524ddd96bac07f03745570b5e1ca9cac20
MechZombie <53657229+MechZombie@users.noreply.github.com>
2019-10-23 22:24:13 -0300

    Add files via upload

---
+++ Tuplas.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#Tuplas são similáres a um vetor, exceto pelo fato de serem imutpaveis
+
+words = ("spam","eggs","sausages")
+# words[0] = "cheese" vai dar erro.
+
+#Outra forma de criar tuplas.
+tuplas = "primeiro","segundo"
+print(tuplas[0])
+
+#É possível atribuir valores dessa forma.
+tupla = (1,(1,2,3))
+print(tupla[1])
